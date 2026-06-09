ce710c179deb8b8a7ef85c5be4831081aec3d274
Vrunda <panchalvrunda853@gmail.com>
2026-06-09 20:06:52 +0530

    Delete routes directory

--- routes/providerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const providerController = require("../controllers/providerController");
-
-router.get("/", providerController.getProviders);
-
-module.exports = router;
--- routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/authMiddleware");
-const reviewController = require("../controllers/reviewController");
-
-router.post("/", verifyToken, reviewController.addReview);
-router.get("/:provider_id", reviewController.getProviderReviews);
-
-module.exports = router;
